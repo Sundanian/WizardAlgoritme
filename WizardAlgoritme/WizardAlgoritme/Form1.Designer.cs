4edee3c5f44dc4ce675bbb2e710001d91f87bbc5
Sundanian <thomas.krog.horne@gmail.com>
2015-12-07 10:26:50 +0100

    Grid Complete

--- WizardAlgoritme/WizardAlgoritme/Form1.Designer.cs
+++ WizardAlgoritme/WizardAlgoritme/Form1.Designer.cs
@@ -29,11 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Loop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Loop
+            // 
+            this.Loop.Enabled = true;
+            this.Loop.Tick += new System.EventHandler(this.Loop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer Loop;
     }
 }
 
